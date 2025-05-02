26665e0bfe2d990984521d1bd522143a681c08bf
Shikhardeep <shikharbhatnagar68@gmail.com>
2025-05-02 17:51:10 +0530

    First commit

---
+++ DockerAssignmentBasic/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DockerAssignmentBasic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerAssignmentBasic.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
